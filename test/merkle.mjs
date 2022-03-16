d6c0ed0c91a9ae15d749c445f416cb66037a9dcc
lsankar4033 <lsankar4033@gmail.com>
2022-03-16 13:12:00 -0700

    unit test for 2-depth merkle tree

--- test/merkle.mjs
+++ test/merkle.mjs
@@ -10,31 +10,55 @@ import { buildTree } from '../src/merkle.mjs';
 
 const wasm_tester = circom_tester.wasm;
 
-describe("merkle tree equivalence", function() {
-  let circuit1;
+async function testLeaves(circuit, leaves) {
+  let { root, leafToPathElements, leafToPathIndices } = await buildTree(leaves);
+  for (const leaf of leaves) {
+    let witness = await circuit.calculateWitness(
+      {
+        leaf: leaf,
+        root: root,
+        pathElements: leafToPathElements[leaf],
+        pathIndices: leafToPathIndices[leaf]
+      },
+      true
+    );
 
+    await circuit.checkConstraints(witness);
+  }
+}
+
+describe("merkle tree equivalence", function() {
   this.timeout(10000000);
 
   it("works for 1-depth merkle tree", async () => {
     const __dirname = dirname(fileURLToPath(import.meta.url));
-    circuit1 = await wasm_tester(path.join(__dirname, "circuits", "merkle_1.circom"));
+    let circuit = await wasm_tester(path.join(__dirname, "circuits", "merkle_1.circom"));
 
-    let leaves = [1, 2];
-    let { root, leafToPathElements, leafToPathIndices } = await buildTree(leaves);
+    let leaves = [32914021943021, 31593205932];
 
-    for (const leaf of leaves) {
-      let witness = await circuit1.calculateWitness(
-        {
-          leaf: leaf,
-          root: root,
-          pathElements: leafToPathElements[leaf],
-          pathIndices: leafToPathIndices[leaf]
-        },
-        true
-      );
-
-      await circuit1.checkConstraints(witness);
-    }
+    await testLeaves(circuit, leaves);
   });
 
+  describe("works for 2-depth merkle tree", () => {
+
+    it("no null elements", async () => {
+      const __dirname = dirname(fileURLToPath(import.meta.url));
+      let circuit = await wasm_tester(path.join(__dirname, "circuits", "merkle_2.circom"));
+
+      let leaves = [58832943290, 9432001023, 9530201010231, 488100101];
+      await testLeaves(circuit, leaves);
+    });
+
+    it.only("single null element (at end)", async () => {
+      const __dirname = dirname(fileURLToPath(import.meta.url));
+      let circuit = await wasm_tester(path.join(__dirname, "circuits", "merkle_2.circom"));
+
+      let leaves = [58832943290, 9432001023, 9530201010231];
+      await testLeaves(circuit, leaves);
+    });
+  });
+
+  //it("works for 10-depth merkle tree using devcon poap values", async () => {
+
+  //});
 });
